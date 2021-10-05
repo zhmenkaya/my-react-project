dc4de7775c84b3729bcc9adef41421699e8f8e9a
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-10-05 21:26:31 +0300

    img and icon in project commit

--- src/components/headliner/Headliner.jsx
+++ src/components/headliner/Headliner.jsx
@@ -1,11 +1,31 @@
 import './Headliner.css';
 
 function Headliner() {
-  return (
-    <div className="headliner">
-      Headliner
-    </div>
-  );
+return (
+    <article className="headliner">
+        <img className="" src="" alt="" />
+        <div className="">
+            <h1 className="">Hate speech vs free speech</h1>
+            <div className="">
+                <div className="">
+                    <img src="" alt="" />
+                    <span>10.8k</span>
+                </div>
+                <div className="">
+                    <img src="" alt="" />
+                    <span>1.4k</span>
+                    </div>
+            </div>
+            <div className="">
+                <span>BBC NEWS</span>
+                <div className="">
+                    <img src="" alt="" />
+                    <span>2:00 pm</span>
+                </div>
+            </div>
+        </div>
+    </article>
+    );
 }
 
 export default Headliner;
