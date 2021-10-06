6b1aff8dc05a50864736ba7088e58e34a88da0d5
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-10-06 16:35:54 +0300

    to add new companent articleHeader in project commit

--- src/components/headline/Headline.jsx
+++ src/components/headline/Headline.jsx
@@ -1,29 +1,29 @@
-import './Headline.css';
+import s from './Headline.module.css';
 import likeIcon from "./../../assets/img/like-icon.svg";
 import dislikeIcon from "./../../assets/img/dislike-icon.svg";
 import clockIcon from "./../../assets/img/clock.svg";
 
 function Headline(props) {
 return (
-    <article className="headline">
-        <img className="thumbnail-img" src={props.thumbnail} alt="" />
-        <div className="info">
-            <h1 className="title">{props.title}</h1>
-            <div className="rating">
-                <div className="like">
-                    <img className="like-icon" src={likeIcon} alt="" />
-                    <span className="text">10.8k</span>
+    <article className={s.headline}>
+        <img className={s.thumbnailImg} src={props.thumbnail} alt="" />
+        <div className={s.info}>
+            <h1 className={s.title}>{props.title}</h1>
+            <div className={s.rating}>
+                <div className={s.like}>
+                    <img className={s.likeIcon} src={likeIcon} alt="" />
+                    <span className={s.text}>10.8k</span>
                 </div>
-                <div className="dislike">
-                    <img className="like-icon" src={dislikeIcon} alt="" />
-                    <span className="text">1.4k</span>
+                <div className={s.dislike}>
+                    <img className={s.likeIcon} src={dislikeIcon} alt="" />
+                    <span className={s.text}>1.4k</span>
                     </div>
             </div>
-            <div className="wrapper">
-                <span style={props.style} className="label">{props.label}</span>
-                <div className="time">
-                    <img  className="time-icon" src={clockIcon} alt="" />
-                    <span className="time-text">2:00 pm</span>
+            <div className={s.wrapper}>
+                <span style={props.style} className={s.label}>{props.label}</span>
+                <div className={s.time}>
+                    <img  className={s.timeIcon} src={clockIcon} alt="" />
+                    <span className={s.timeText}>2:00 pm</span>
                 </div>
             </div>
         </div>
