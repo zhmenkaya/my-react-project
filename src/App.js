ba7121b41f40f1b013e9591d147d95d9a99b952e
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-10-06 19:36:59 +0300

    new structure with classname in project commit

--- src/App.js
+++ src/App.js
@@ -10,12 +10,26 @@ import img6 from './assets/img/image6.jpg';
 function App() {
   return (
     <div className="App">
-      <Headline style={{backgroundColor: "lightgreen"}} title="Hate speech vs free speech" thumbnail={img1} label="BBC NEWS"/>
-      <Headline title="Ontario Liberal leadership" thumbnail={img2} label="BBC NEWS"/>
-      <Headline title="Victim of Scarborough" thumbnail={img3} label="BBC NEWS"/>
-      <Headline title="Mother who left her baby" thumbnail={img4} label="ABC NEWS"/>
-      <Headline title="Mother of Stefanie Rengel" thumbnail={img5} label="CW6 NEWS"/>
-      <Headline title="World   Cup: Popular items" thumbnail={img6} label="CNN NEWS"/>
+      <header className="header">header</header>
+      <aside className="sidebar">aside</aside>
+      <main className="main">
+        <section className="headlines">
+          <h2 className="block-title">Today’s Headlines</h2>
+          <div className="block-content">
+            <Headline style={{backgroundColor: "lightgreen"}} title="Hate speech vs free speech" thumbnail={img1} label="BBC NEWS"/>
+            <Headline title="Ontario Liberal leadership" thumbnail={img2} label="BBC NEWS"/>
+            <Headline title="Victim of Scarborough" thumbnail={img3} label="BBC NEWS"/>
+            <Headline title="Mother who left her baby" thumbnail={img4} label="ABC NEWS"/>
+            <Headline title="Mother of Stefanie Rengel" thumbnail={img5} label="CW6 NEWS"/>
+            <Headline title="World   Cup: Popular items" thumbnail={img6} label="CNN NEWS"/>
+          </div>
+        </section>
+
+      </main>
+
+
+
+      
     </div>
   );
 }
